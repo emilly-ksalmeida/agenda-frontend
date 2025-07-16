868df7c545356d9aa9c6960ab7cb1a124bd9b2d7
Emilly K S Almeida <emilly.eksa@gmail.com>
2025-07-16 18:27:13 -0300

    Ajustes no CSS.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,13 @@
 function NavBar(){
 
     return(
-       <nav className="space-y-4 p-5 bg-slate-800 rounded-md shadow flex gap-2">
+       <nav className="p-5 bg-slate-800 rounded-md shadow flex gap-2 justify-between items-center">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Agenda</h1>
-        <button className="bg-slate-400 text-white p-2 rounded-md">Criar Conta</button>
-        <button className="bg-slate-400 text-white p-2 rounded-md">Login</button>
+        <div className="flex gap-2 mt-0">
+            <button className="bg-slate-400 text-white p-2 rounded-md">Criar Conta</button>
+            <button className="bg-slate-400 text-white p-2 rounded-md">Login</button>
+        </div>
+        
        </nav>
     )
 }
