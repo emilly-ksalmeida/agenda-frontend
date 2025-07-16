9fdbd4d94972689368ec72cadd6993fce70a29b8
Emilly K S Almeida <emilly.eksa@gmail.com>
2025-07-16 17:31:35 -0300

    Adicionando botões para funcionalidades futuras.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,10 @@
 function NavBar(){
 
     return(
-       <nav className="space-y-4 p-5 bg-slate-800 rounded-md shadow">
+       <nav className="space-y-4 p-5 bg-slate-800 rounded-md shadow flex gap-2">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Agenda</h1>
+        <button className="bg-slate-400 text-white p-2 rounded-md">Criar Conta</button>
+        <button className="bg-slate-400 text-white p-2 rounded-md">Login</button>
        </nav>
     )
 }
