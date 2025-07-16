4cd0c42595106f212c4232b499ab00b8cfe72998
Emilly K S Almeida <emilly.eksa@gmail.com>
2025-07-16 17:54:21 -0300

    Adicionando título e novo componente para criar contatos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import AddContact from "./components/AddContact";
 
 function App() {
   
@@ -8,6 +9,10 @@ function App() {
     <div className="w-screen h-screen bg-slate-500 flex justify-center">
       <div className="w-[900px] space-y-4">
       <NavBar />
+      <h1 className="text-3xl text-slate-100 font-bold text-center">Contatos</h1>
+      
+      <AddContact />
+      
       </div>
     </div>
   )
