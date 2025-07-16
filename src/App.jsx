ab9f3b748d3000e466e832a3533399c3870bd0ec
Emilly K S Almeida <emilly.eksa@gmail.com>
2025-07-16 17:20:48 -0300

    Adicionando estrutura inicial do componente NavBar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   
 
   return (
-    <h1>Agenda</h1>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center">
+      <div className="w-[900px] space-y-4">
+      <NavBar />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+function NavBar(){
+
+    return(
+       <nav className="space-y-4 p-5 bg-slate-800 rounded-md shadow">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">Agenda</h1>
+       </nav>
+    )
+}
+export default NavBar;
