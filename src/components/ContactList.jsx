026d65d6c0f11aac0bb988f494d08e378a9f95ba
Emilly K S Almeida <emilly.eksa@gmail.com>
2025-07-17 09:39:05 -0300

    Renderizando a lista de contatos.

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,14 +1,20 @@
-function ContactList(){
-    
+function ContactList(props){
     return (
         <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
             <h2 className="text-1xl text-slate-600 text-left">Lista de Contatos</h2>
-            <ul>
-                <li className="flex gap-2">
-                <p className="bg-slate-400 text-white p-2 rounded-md">Nome</p>
-                <button className="bg-slate-400 text-white p-2 rounded-md">Detalhes</button>
-                <button className="bg-slate-400 text-white p-2 rounded-md">Excluir</button>       
-                </li>
+            <ul className="space-y-4">
+                {props.list.map((contato)=>(
+                    <li key={contato.id} className="flex gap-2">
+                    
+                    <div>
+                    <p className="bg-slate-500 text-white p-2 rounded-md">{contato.nome}</p>
+                    <p className="bg-slate-500 text-white p-2 rounded-md">{contato.email}</p>
+                    </div>
+
+                    <button className="bg-slate-500 text-white p-2 rounded-md">Detalhes</button>
+                    <button className="bg-slate-500 text-white p-2 rounded-md">Excluir</button>       
+                    </li>
+                ))}
             
             </ul>
         </div>
