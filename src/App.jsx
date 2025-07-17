d8f3163e7bd2df605d2c891e979f4c0de6977aca
Emilly K S Almeida <emilly.eksa@gmail.com>
2025-07-17 15:06:38 -0300

    Adicionando as funções necessárias para salvar os novos dados de contato.

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,26 @@ function App() {
     }
   ]);
 
+  function onAddContact (firstName, lastName, email, phone){
+    const newList = {
+      id: list.length + 1,
+      firstName,
+      lastName,
+      email,
+      phone
+    };
+    setList([...list, newList]);
+  }
+console.log(list);
   return (
     <div className="w-screen bg-slate-500 flex justify-center py-2">
       <div className="w-[900px] space-y-4">
       <NavBar />
       <h1 className="text-3xl text-slate-100 font-bold text-center">Contatos</h1>
       
-      <AddContact />
+      <AddContact
+      onAddContact={onAddContact}
+      />
 
       <ContactList
       list={list}
--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,15 +1,56 @@
-function AddContact(){
-   
+import { useState } from "react";
+
+function AddContact({ onAddContact }){
+   const [firstName, setFirstName] = useState("");
+   const [lastName, setLastName] = useState("");
+   const [email, setEmail] = useState("");
+   const [phone, setPhone] = useState("");
+
+
     return (
         <div className="space-y-4 p-4 bg-slate-200 rounded-md shadow flex flex-col">
             <h2 className="text-1xl text-slate-600 text-left">Novo Contato:</h2>
-            <input type="text" placeholder="Nome" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" />
-            <input type="text" placeholder="Sobrenome" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" />
-            <input type="email" placeholder="E-mail" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" />
-            <input type="number" placeholder="Telefone" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" />
+            <input
+            type="text"
+            placeholder="Nome"
+            value={firstName}
+            className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+            onChange={(event)=> setFirstName(event.target.value)}
+            />
+            <input
+            type="text"
+            placeholder="Sobrenome"
+            value={lastName}
+            className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+            onChange={(event)=> setLastName(event.target.value)}
+            />
+            <input
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+            onChange={(event)=> setEmail(event.target.value)}
+            />
+            <input
+            type="number"
+            placeholder="Telefone"
+            value={phone}
+            className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+            onChange={(event)=> setPhone(event.target.value)}
+            />
+            
             <button
             className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
-            >Adicionar</button>
+            onClick={()=>{
+                onAddContact(firstName, lastName, email, phone);
+                setFirstName("");
+                setLastName("");
+                setEmail("");
+                setPhone("");  
+            }}
+            >
+                Adicionar
+            </button>
 
         </div>
     )
