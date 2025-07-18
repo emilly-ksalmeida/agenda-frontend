b98b0529214be431a6ea62ffdfd13f6a99c2b15d
Emilly K S Almeida <emilly.eksa@gmail.com>
2025-07-18 09:43:32 -0300

    Inserindo alguns dados de teste via query params para serem renderizados.

--- src/pages/ContactDetails.jsx
+++ src/pages/ContactDetails.jsx
@@ -1,17 +1,20 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function ContactDetails(){
     
     const navigate = useNavigate();
-   
+    const [searchParams] = useSearchParams();
+    const firstName = searchParams.get("firstName");
+    const lastName = searchParams.get("lastName");
+
     return (
        <div className="h-screen w-screen bg-slate-500 p-6">
             <div className="w-[500px] mx-auto space-y-4">
                 <div className="flex justify-center relative mb-6">
 
                     <button
-                    className="absolute left-0 top-0 bottom-0 bg-slate-700"
-                    onClick={()=> navigate(-1)}>
+                    className="bg-slate-800 text-slate-100 p-2 rounded-md"
+                    onClick={()=> navigate(-1)}>Voltar
                     </button>
 
                     <h1 className="text-3xl text-slate-100 font-bold text-center">
@@ -21,8 +24,8 @@ function ContactDetails(){
                 </div>
 
                 <div className="bg-slate-200 p-4 rounded-md">
-                    <p className="text-slate-600">firstName</p>
-                    <p className="text-slate-600">lastName</p>
+                    <p className="text-slate-600">{firstName}</p>
+                    <p className="text-slate-600">{lastName}</p>
                     <p className="text-slate-600">email</p>
                     <p className="text-slate-600">phone</p>
                 </div>
