f0f4da99c6fbbf3c8eef4583044fb562d8089b59
Emilly K S Almeida <emilly.eksa@gmail.com>
2025-07-18 09:41:30 -0300

    Adicionando rota para página de detalhes do contato.

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,4 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
+
 function ContactList({ list, onClickDelete }){
+
+    const navigate = useNavigate();
+
+    function onSeeDetailsClick(contact){
+        const query = new URLSearchParams();
+        query.set("firstName", contact.firstName);
+        query.set("lastName", contact.lastName);
+        navigate(`/contact-details?${query.toString()}`);
+    }
+
     return (
         <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
             <h2 className="text-1xl text-slate-600 text-left">Lista de Contatos</h2>
@@ -11,7 +24,10 @@ function ContactList({ list, onClickDelete }){
                     <p className="bg-slate-500 text-white p-2 rounded-md">{contact.email}</p>
                     </div>
 
-                    <button className="bg-slate-500 text-white p-2 rounded-md">Detalhes</button>
+                    <button className="bg-slate-500 text-white p-2 rounded-md"
+                        onClick={() => onSeeDetailsClick(contact)}
+                    >Detalhes
+                    </button>
 
                     <button
                     className="bg-slate-500 text-white p-2 rounded-md"
