4e77e16b335c5bc55fe020f99771d5ca5389f4e1
Emilly K S Almeida <emilly.eksa@gmail.com>
2025-07-22 10:13:41 -0300

    Transferindo useEffect para o arquivo onde o estado list é definido.

--- src/components/AppProvider.jsx
+++ src/components/AppProvider.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AppContext from "../AppContext";
 
 function AppProvider(props){
-    const [list, setList] = useState(
+  const [list, setList] = useState(
     JSON.parse(localStorage.getItem("contactList")) || []
   );
+  useEffect(()=>{
+    localStorage.setItem("contactList", JSON.stringify(list));
+
+  }, [list]);
 
     return(
         <AppContext.Provider value={[list, setList]}>
