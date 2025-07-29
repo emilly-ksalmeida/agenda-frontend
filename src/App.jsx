27c35ea33f0d57f86803ec2ad59450fdce2c3973
Emilly K S Almeida <emilly.eksa@gmail.com>
2025-07-29 12:23:33 -0300

    Ajustes de style.

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   }
 
   return (
-    <div className="w-screen bg-slate-500 flex justify-center py-2">
+    <div className="w-screen min-h-screen bg-slate-500 flex justify-center py-2">
       <div className="w-[900px] space-y-4">
       <NavBar />
       <h1 className="text-3xl text-slate-100 font-bold text-center">Contatos</h1>
